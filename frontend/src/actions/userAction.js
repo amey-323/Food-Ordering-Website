e1089e1be5e8b9bd2557ed2f3539c9f19eaab9b7
amey-323 <62855667+amey-323@users.noreply.github.com>
2021-11-10 21:54:38 +0530

    error resolved

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -29,7 +29,7 @@ export const register = (userData) => async (dispatch) => {
   try {
     dispatch({ type: REGISTER_USER_REQUEST });
     const config = { headers: { "Content-type": "multipart/form-data" } };
-    const { data } = await axios.post(`api/v1/register`, { userData }, config);
+    const { data } = await axios.post(`api/v1/register`, userData, config);
     dispatch({ type: REGISTER_USER_SUCCESS, payload: data.user });
   } catch (error) {
     dispatch({ type: REGISTER_USER_FAIL, payload: error.response.data.message });
