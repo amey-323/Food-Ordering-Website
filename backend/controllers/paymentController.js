6028e08fc86eb3c955268881bd5d65c8d697b52b
amey-323 <amey.gangan@somaiya.edu>
2022-07-22 15:44:00 +0530

    new updated

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -1,22 +1,24 @@
 //Payment
-const catchAsyncErrors = require('../middleware/catchAsyncErrors')
-
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+require("dotenv").config({ path: "backend/config/config.env" });
+const stripe = require("stripe")(
+  "sk_test_51JwoQ1SIQ5NQ03bdFTi7xMCRbclRA36Tz5UIgOCDMojSPgbPCaiUuLq83UEJO0x0dhFMbgAxV3vfHnSEWPhnzTLr00OI6Bbxmm"
+);
 
 exports.processPayment = catchAsyncErrors(async (req, res, next) => {
   const myPayment = await stripe.paymentIntents.create({
     amount: req.body.amount,
-    currency: 'inr',
+    currency: "inr",
     metadata: {
-      company: 'FoodApp',
+      company: "FoodApp",
     },
-  })
+  });
 
   res
     .status(200)
-    .json({ success: true, client_secret: myPayment.client_secret })
-})
+    .json({ success: true, client_secret: myPayment.client_secret });
+});
 
 exports.sendStripeApiKey = catchAsyncErrors(async (req, res, next) => {
-  res.status(200).json({ stripeApiKey: process.env.STRIPE_API_KEY })
-})
+  res.status(200).json({ stripeApiKey: process.env.STRIPE_API_KEY });
+});
